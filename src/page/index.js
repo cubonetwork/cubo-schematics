aba98340c15992dd489d6de571dc4748a6524499
LFeh <hi@felipefialho.com>
2018-04-23 11:12:00 -0300

    Fix .gitignore

---
+++ src/page/index.js
@@ -0,0 +1,60 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+const core_1 = require("@angular-devkit/core");
+const schematics_1 = require("@angular-devkit/schematics");
+const ts = require("typescript");
+const ast_utils_1 = require("../utility/ast-utils");
+const change_1 = require("../utility/change");
+const config_1 = require("../utility/config");
+const find_module_1 = require("../utility/find-module");
+const parse_name_1 = require("../utility/parse-name");
+const validation_1 = require("../utility/validation");
+function buildSelector(options, projectPrefix) {
+    let selector = core_1.strings.dasherize(options.name);
+    if (options.prefix) {
+        selector = `${options.prefix}-${selector}`;
+    }
+    else if (projectPrefix) {
+        selector = `${projectPrefix}-${selector}`;
+    }
+    return selector;
+}
+function default_1(options) {
+    return (host, context) => {
+        const workspace = config_1.getWorkspace(host);
+        if (!options.project) {
+            options.project = Object.keys(workspace.projects)[0];
+        }
+        const project = workspace.projects[options.project];
+        if (options.path === undefined) {
+            options.path = `/${project.root}/src/app`;
+        }
+        options.module = find_module_1.findModuleFromOptions(host, options);
+        const parsedPath = parse_name_1.parseName(options.path, options.name);
+        options.name = parsedPath.name;
+        options.path = parsedPath.path;
+        options.selector = options.selector || buildSelector(options, project.prefix);
+        validation_1.validateName(options.name);
+        validation_1.validateHtmlSelector(options.selector);
+        const templateSource = schematics_1.apply(schematics_1.url('./files'), [
+            options.spec ? schematics_1.noop() : schematics_1.filter(path => !path.endsWith('.spec.ts')),
+            options.inlineStyle ? schematics_1.filter(path => !path.endsWith('.__styleext__')) : schematics_1.noop(),
+            options.inlineTemplate || options.root ? schematics_1.filter(path => !path.endsWith('.html')) : schematics_1.noop(),
+            schematics_1.template(Object.assign({}, core_1.strings, { 'if-flat': (s) => options.flat ? '' : s }, options)),
+            schematics_1.move(parsedPath.path),
+        ]);
+        return schematics_1.chain([
+            schematics_1.branchAndMerge(schematics_1.chain([
+                schematics_1.mergeWith(templateSource),
+            ])),
+        ])(host, context);
+    };
+}
+exports.default = default_1;
