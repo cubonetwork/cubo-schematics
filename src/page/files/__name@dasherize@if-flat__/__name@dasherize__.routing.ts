d8fa0dadfc5f2094ca7d235608e732557575a668
LFeh <hi@felipefialho.com>
2018-05-22 11:09:43 -0300

    Remove page prefix

--- src/page/files/__name@dasherize@if-flat__/__name@dasherize__.routing.ts
+++ src/page/files/__name@dasherize@if-flat__/__name@dasherize__.routing.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { Page<%= classify(name) %>Component } from './<%= dasherize(name) %>.component';
+import { <%= classify(name) %>Component } from './<%= dasherize(name) %>.component';
 
-export const Page<%= classify(name) %>Routes: Routes = [
+export const <%= classify(name) %>Routes: Routes = [
   {
     path: '',
-    component: Page<%= classify(name) %>Component,<% if(root) { %>
+    component: <%= classify(name) %>Component,<% if(root) { %>
     children: [
       {
         path: 'childrenpath',
@@ -15,8 +15,8 @@ export const Page<%= classify(name) %>Routes: Routes = [
   }
 ];
 @NgModule({
-  <% if(root) { %>imports: [RouterModule.forRoot(Page<%= classify(name) %>Routes)],
-  <% } else { %>imports: [RouterModule.forChild(Page<%= classify(name) %>Routes)],
+  <% if(root) { %>imports: [RouterModule.forRoot(<%= classify(name) %>Routes)],
+  <% } else { %>imports: [RouterModule.forChild(<%= classify(name) %>Routes)],
   <% } %>exports: [RouterModule]
 })
-export class Page<%= classify(name) %>Routing { }
+export class <%= classify(name) %>Routing { }
